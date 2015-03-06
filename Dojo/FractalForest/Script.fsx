15aa64312d9af350df5cda42a3c4bf4cace8cf9d
Ryan Riley <ryan@tachyus.com>
2015-03-05 22:33:17 -0600

    Built recursively with color

--- Dojo/FractalForest/Script.fsx
+++ Dojo/FractalForest/Script.fsx
@@ -3,7 +3,7 @@ open System.Drawing
 open System.Windows.Forms
 
 // Create a form to display the graphics
-let width, height = 500, 500         
+let width, height = 500, 500
 let form = new Form(Width = width, Height = height)
 let box = new PictureBox(BackColor = Color.White, Dock = DockStyle.Fill)
 let image = new Bitmap(width, height)
@@ -12,11 +12,10 @@ let graphics = Graphics.FromImage(image)
 //at the expense of speed. Uncomment it if you want
 //more beautiful images, even if it's slower.
 //Thanks to https://twitter.com/AlexKozhemiakin for the tip!
-//graphics.SmoothingMode <- System.Drawing.Drawing2D.SmoothingMode.HighQuality
-let brush = new SolidBrush(Color.FromArgb(0, 0, 0))
+graphics.SmoothingMode <- System.Drawing.Drawing2D.SmoothingMode.HighQuality
 
 box.Image <- image
-form.Controls.Add(box) 
+form.Controls.Add(box)
 
 // Compute the endpoint of a line
 // starting at x, y, going at a certain angle
@@ -40,17 +39,31 @@ let drawLine (target : Graphics) (brush : Brush)
     target.DrawLine(pen, origin, destination)
 
 let draw x y angle length width = 
+    let red = max 0 (int length - 20)
+    let brush = new SolidBrush(Color.FromArgb(red, 100, 50))
     drawLine graphics brush x y angle length width
 
 let pi = Math.PI
 
 // Now... your turn to draw
 // The trunk
-draw 250. 50. (pi*(0.5)) 100. 4.
-let x, y = endpoint 250. 50. (pi*(0.5)) 100.
+//draw 250. 50. (pi*(0.5)) 100. 4.
+//let x, y = endpoint 250. 50. (pi*(0.5)) 100.
 // first and second branches
-draw x y (pi*(0.5 + 0.3)) 50. 2.
-draw x y (pi*(0.5 - 0.4)) 50. 2.
+//draw x y (pi*(0.5 + 0.3)) 50. 2.
+//draw x y (pi*(0.5 - 0.4)) 50. 2.
+
+let drawTree x y angle length width =
+    let rec drawBranch x y angle length width div =
+        if div > 1. then
+            draw x y (pi*angle) length width
+            let x', y' = endpoint x y (pi*angle) length
+            let length', width', div' = length/div, width/div, div*0.95
+            drawBranch x' y' (angle + (0.3*0.82)) length' width' div'
+            drawBranch x' y' (angle - (0.4*0.78)) length' width' div'
+        else ()
+    drawBranch x y angle length width 2.
+drawTree 250. 50. 0.5 200. 5.
 
 form.ShowDialog()
 
